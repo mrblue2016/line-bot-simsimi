ff6bc6153032c6980c971e541f1c998fc52bcb26
mrblue2016 <46017306+mrblue2016@users.noreply.github.com>
2018-12-20 18:37:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,22 +1,13 @@
 <?php
-/*
-copyright @ medantechno.com
-Modified by Ilyasa
-2017
-*/
 require_once('./line_class.php');
-
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
-$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
-
+$channelAccessToken = 'xq1ZicEYD8t+gkFSePswCnb2EVf/bo6fj4tB3VySMuWtZRUp6prf9TOwAYYp4tNDlACdpglyR0Yx3WCeCWplXWbyLYqi/jlyNvpFJ1+npW5c9q1EIii4iQciPDkfPIxhquEHP5OSYL6dLxEU6T3fowdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = '068ed74c96a8b5bc4c066fce61209e7c';//Your Channel Secret
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
 $userId 	= $client->parseEvents()[0]['source']['userId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
 $message 	= $client->parseEvents()[0]['message'];
 $profil = $client->profil($userId);
 $pesan_datang = $message['text'];
-
 if($message['type']=='sticker')
 {	
 	$balas = array(
@@ -34,7 +25,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = 'fc8899c9656d4107356dc878c9854884'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
@@ -88,8 +79,5 @@ if($url['result'] != 100)
 }
  
 $result =  json_encode($balas);
-
 file_put_contents('./reply.json',$result);
-
-
 $client->replyMessage($balas);
